72d6a74bb9b3ad897820b6662121dfbcc16ee349
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-05-19 15:33:50 +0200

    showMeasurement function

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const trainingPlanBtn = document.querySelector('.plan-btn');
 const trainingPlan = document.querySelector('.training-plan')
 const closeBtn = document.querySelector('.fas')
+const measurent = document.querySelector('.measurement-form')
+const measurementBtn = document.querySelector('.measurement-btn')
+const closeMeasBtn = document.querySelector('.fas-close')
 
 const showPlan = () => {
     trainingPlan.classList.add('active')
 }
 
-
 const closePlan = () => {
     trainingPlan.classList.remove('active')
 }
+
+const showMeasurent = () => {
+    measurent.classList.add('active')
+}
+
+const closeMeas = () => {
+    measurent.classList.remove('active')
+}
 trainingPlanBtn.addEventListener('click', showPlan);
-closeBtn.addEventListener('click', closePlan)
+closeBtn.addEventListener('click', closePlan);
+measurementBtn.addEventListener('click', showMeasurent);
+closeMeasBtn.addEventListener('click', closeMeas)
