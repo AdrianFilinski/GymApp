e9d1423c11c59aca57eaffdf65b262b78ac72f23
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-05-22 12:10:13 +0200

    add showPlan function

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-const trainingPlanBtn = document.querySelector('.plan-btn');
+const trainingPlanArea = document.querySelector('.training-plan-area')
 const trainingPlan = document.querySelector('.training-plan')
-const closeBtn = document.querySelector('.fas')
-const measurent = document.querySelector('.measurement-form')
-const measurementBtn = document.querySelector('.measurement-btn')
-const closeMeasBtn = document.querySelector('.fas-close')
 
 const showPlan = () => {
-    trainingPlan.classList.add('active')
+    trainingPlanArea.style.display = 'flex';
 }
 
-const closePlan = () => {
-    trainingPlan.classList.remove('active')
-}
 
-const showMeasurent = () => {
-    measurent.classList.add('active')
-}
-
-const closeMeas = () => {
-    measurent.classList.remove('active')
-}
-trainingPlanBtn.addEventListener('click', showPlan);
-closeBtn.addEventListener('click', closePlan);
-measurementBtn.addEventListener('click', showMeasurent);
-closeMeasBtn.addEventListener('click', closeMeas)
+trainingPlan.addEventListener('click', showPlan);
