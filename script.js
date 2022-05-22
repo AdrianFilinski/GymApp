98b8f6d15c77654235cee9e4ff8291f5beb7d93c
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-05-22 12:25:09 +0200

    create closePlan function but it's not perfect...

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const trainingPlanArea = document.querySelector('.training-plan-area')
-const trainingPlan = document.querySelector('.training-plan')
+const trainingPlanArea = document.querySelector('.training-plan-area');
+const trainingPlan = document.querySelector('.training-plan');
+const closeBtn = document.querySelector('.close');
 
 const showPlan = () => {
     trainingPlanArea.style.display = 'flex';
 }
 
+const closePlan = () => {
+    trainingPlanArea.style.display = 'none'
+}
 
 trainingPlan.addEventListener('click', showPlan);
+closeBtn.addEventListener('click', closePlan)
+
