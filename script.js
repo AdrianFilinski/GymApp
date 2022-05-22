b0cf5efe726fc9f9e2432105b5a47c088871eb9f
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2022-05-22 17:09:27 +0200

    add training function with buttons

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const trainingPlanArea = document.querySelector('.training-plan-area');
 const trainingPlan = document.querySelector('.training-plan');
 const closeBtn = document.querySelector('.close');
+const startTraining = document.querySelector('.start-training')
+const trainingPanel = document.querySelector('.training-area')
+const header = document.querySelector('header');
+const closeTrainingBtn = document.querySelector('.close-training')
 
 const showPlan = () => {
     trainingPlanArea.style.display = 'flex';
@@ -10,6 +14,17 @@ const closePlan = () => {
     trainingPlanArea.style.display = 'none'
 }
 
-trainingPlan.addEventListener('click', showPlan);
-closeBtn.addEventListener('click', closePlan)
+const runTraining = () => {
+    trainingPanel.style.display = 'flex';
+    header.style.display = 'none';
+}
 
+const closeTraining = () => {
+    trainingPanel.style.display = 'none';
+    header.style.display = 'flex';
+}
+
+trainingPlan.addEventListener('click', showPlan);
+closeBtn.addEventListener('click', closePlan);
+startTraining.addEventListener('click', runTraining);
+closeTrainingBtn.addEventListener('click', closeTraining);
